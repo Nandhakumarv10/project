b11bb3190c1b3157efd59b5d06561502e4ef356a
Nandhakumarv10 <nandhakumar652001@gmail.com>
2025-02-05 14:51:57 +0530

    working on react basics

---
+++ react-tasks/src/app/MyButton/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function MyButton() {
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+       <button
+    className="p-2 bg-blue-600 rounded-lg
+    "
+    >Click me</button>
+    </div>
+  )
+}
---
+++ react-tasks/src/app/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import MyButton from './MyButton/page'
+
+export default function Page() {
+  return (
+    <div className="bg-white text-black w-full h-screen">
+      <MyButton />
+    </div>
+  )
+}
